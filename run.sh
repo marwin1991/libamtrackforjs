02d9d9c5c96ec0831c98d6a6c69669c435d5246e
marwin1991 <peter.zmilczak@gmail.com>
2018-04-14 15:56:55 -0400

    run.sh modification

--- run.sh
+++ run.sh
@@ -11,11 +11,41 @@ cd _build
 "$emcmake" cmake ..
 "$emmake" make
 
-#AT.h
+#----AT.h
 funs='["_AT_test_fun",'
 
-#AT_DataMaterial.h
-funs+='"_AT_material_index_from_material_number"'
+#----AT_DataMaterial.h
+funs+='"_AT_material_index_from_material_number",'
+funs+='"_AT_material_name_from_number",'
+funs+='"_AT_material_number_from_name",'
+funs+='"_AT_density_g_cm3_from_material_no",'
+funs+='"_AT_I_eV_from_material_no",'
+funs+='"_AT_alpha_g_cm2_MeV_from_material_no",'
+funs+='"_AT_p_MeV_from_material_no",'
+funs+='"_AT_m_g_cm2_from_material_no",'
+funs+='"_AT_average_A_from_material_no",'
+funs+='"_AT_average_Z_from_material_no",'
+funs+='"_AT_phase_from_material_no",'
+funs+='"_AT_get_material_data",'
+funs+='"_AT_get_materials_data",'
+funs+='"_AT_electron_density_m3_from_material_no_single",'
+funs+='"_AT_electron_density_m3_from_material_no_multi",'
+funs+='"_AT_plasma_energy_J_from_material_no",'
+funs+='"_AT_electron_density_m3_single",'
+funs+='"_AT_electron_density_m3_multi",'
+funs+='"_AT_plasma_energy_J_single",'
+funs+='"_AT_electron_density_m3_from_composition",'
+funs+='"_AT_average_A_from_composition",'
+funs+='"_AT_average_Z_from_composition",'
+funs+='"_AT_effective_Z_from_composition",'
+funs+='"_AT_I_eV_from_composition",'
+funs+='"_AT_set_user_material",'
+funs+='"_AT_set_user_material_from_composition"'
+
+#----AT_DataPartical.h
+#funs+='"_",'
+
+
 funs+=']'
 
 
